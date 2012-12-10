ca0c9d518d0cc8bcfe0272a5a508ef32638505e5
LZY Lenovo <zhaoyu.luo@jiepang.com>
2012-12-10 17:05:47 +0800

    read trello cards

--- content.js
+++ content.js
@@ -1,9 +1,13 @@
 var trello;
 init = function() {
     console.log('content.js initialized');
-    var content_element = document.getElementsByTagName('body').item();
-    trello = eval(content_element);
-    console.log('trello' + trello);
+    var content_element = document.getElementsByTagName('body').item().textContent;
+    trello = eval('(' + content_element + ')');
+    trello_cards = trello.cards;
+    for (var i in trello_cards) {
+           console.log(trello_cards[i]['name'] + "\t");
+    }
+    console.log('done');
 }
 
 init();
